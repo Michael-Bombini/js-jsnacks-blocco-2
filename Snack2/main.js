04813ae9f5445de0b4edb07238d01586ad4db2c9
Michael Bombini <bombini.michael@gmail.com>
2022-05-24 13:59:23 +0200

    Completed Snack2

---
+++ Snack2/main.js
@@ -0,0 +1,26 @@
+/*
+Snack2
+Generatore di “nomi cognomi” casuali: 
+il Grande Gatsby ha  una lista di nomi e una lista di cognomi, 
+e da queste vuole generare una falsa lista di invitati con nome e cognome.
+*/
+
+const nomi = ["Mario", "Luigi", "Giovanni", "Giuseppe", "Pippo", "Pluto", "Gatsby"];
+const cognomi = ["Rossi", "Verdi", "Gialli", "Conte", "Bianchi", "Gallo", "Verde"];
+const generato = [];
+
+const quantiGenerare = parseInt(prompt(`Il grande Gatsby ti chiede quanti Nomi e cognomi vuoi generare?`))
+console.log(quantiGenerare);
+const lista = document.getElementById("lista");
+const maxNomi = nomi.length - 1;
+const maxCognomi = cognomi.length - 1;
+
+for (let i = 0; i < quantiGenerare; i++) {
+    let indiceNome = Math.floor(Math.random() * maxNomi);
+    let indiceCognome = Math.floor(Math.random() * maxCognomi)
+    generato.push(nomi[indiceNome] + " " + cognomi[indiceCognome]);
+    lista.innerHTML += `<li>${generato[i]}</li>`
+    console.log(generato);
+
+}
+
